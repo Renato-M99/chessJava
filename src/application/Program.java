f481f1e6f24b73c1e72c8549397472cd909a104d
Renato Martins <renatojunior7@hotmail.com>
2023-03-23 14:56:46 -0300

    primeira classe criada, position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("começo");
+	
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
