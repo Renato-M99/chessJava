f01f270ecf49abbf6e285b64b05e16c9b98c4162
Renato Martins <renatojunior7@hotmail.com>
2023-03-23 15:37:17 -0300

    camada chess e mostrando o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -3,12 +3,12 @@ package boardgame;
 public class Board {
 	
 	private int rows;
-	private int column;
+	private int columns;
 	private Piece[][] pieces;
 	
 	public Board(int rows, int column) {
 		this.rows = rows;
-		this.column = column;
+		this.columns = column;
 		pieces = new Piece[rows][column];
 	}
 
@@ -20,15 +20,20 @@ public class Board {
 		this.rows = rows;
 	}
 
-	public int getColumn() {
-		return column;
+	public int getColumns() {
+		return columns;
 	}
 
-	public void setColumn(int column) {
-		this.column = column;
+	public void setColumns(int column) {
+		this.columns = column;
 	}
 	
+	public Piece piece (int row, int column) {
+		return pieces [row][column];
+	}
 	
-	
+	public Piece piece (Position position) {
+		return pieces [position.getRow()][position.getColumn()];
+	}
 
 }
