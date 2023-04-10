75bb3d7e6771499d8bffe2feee0f55d950d3b90f
Renato Martins <renatojunior7@hotmail.com>
2023-04-10 17:48:12 -0300

    Cavalo

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -33,7 +33,7 @@ public class Bishop extends ChessPiece {
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() - 1, p.getColumn() + 1);
-			;
+			
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -50,7 +50,6 @@ public class Bishop extends ChessPiece {
 		}
 
 		// diagonal direita de baixo
-		
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
